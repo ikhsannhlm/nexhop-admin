93fc1951f780777f2491a5b2c39422aa98a8b31f
ikhsannhlm <muhammad.ikhsan.n0192@gmail.com>
2023-12-31 00:16:19 +0700

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 import '@/styles/globals.css'
+import { SessionProvider } from "next-auth/react"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Poppins } from 'next/font/google';
+import { Toaster } from 'react-hot-toast';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const inter = Poppins({
+  subsets: ['latin'],
+  weight: '400'
+});
+
+export default function App({
+  Component, pageProps: { session, ...pageProps }
+}) {
+  return (
+    <SessionProvider session={session}>
+      <main className={`${inter.className}`}>
+        <Header />
+        <div className="min-h-screen max-w-screen-2xl mx-auto">
+          <Component {...pageProps} />
+          <Toaster position='top-center'  />
+        </div>
+        <Footer />
+      </main>
+    </SessionProvider>
+  )
 }
